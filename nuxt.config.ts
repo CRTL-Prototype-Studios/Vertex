215c2650a6f6fc62f92fbb6c6b377cba114829ad
Type-32 <87076491+Type-32@users.noreply.github.com>
2024-09-24 09:57:09 +0800

    feat: adding packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  future: {
+    compatibilityVersion: 4,
+  },
+  nitro: {
+    experimental: {
+      tasks: true,
+    },
+  },
   devtools: { enabled: true },
-  modules: ["@nuxt/ui"]
+  modules: ["@nuxt/ui", "nuxt-tiptap-editor", "@sidebase/nuxt-auth"],
+  compatibilityDate: "2024-09-24",
+  tiptap: {
+    prefix: 'Tiptap', //prefix for Tiptap imports, composables not included
+  },
 })
